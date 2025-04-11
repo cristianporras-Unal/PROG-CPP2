c084b0d8fe39e7f931e301ebb6260c6b3ddf491a
cristianporras-Unal <cporrasm@unal.edu.com>
2025-04-11 07:45:56 -0500

    se agegra carpeta de la clase

---
+++ 2025-04-11clase/Juego parte 2.cpp
@@ -0,0 +1,36 @@
+/*
+juego adivinar numero
+
+Tner Un numero
+Preguntar al usuario el numero
+Informar si lo adivino
+!= OPERADOR LOGICO DE DESIGUALDad
+condiciones logicas if else etc
+clase 04 11
+
+e307== 10 por 10 al 307
+*/
+#include <iostream>
+
+  int main(void ){
+
+     const int NUM = 10; // 
+     int guessed_number = NUM/2;
+  
+    while(guessed_number != NUM){
+       std::cout << "Adivina un Numero entre 1 y 100:\n";
+       std::cin >> guessed_number;
+
+        if (guessed_number == NUM){
+       std::cout << "GANASTE!!!\n";
+        //break;
+       } else if (guessed_number > NUM){
+        std:: cout << "Te pasaste.\n"; 
+       } 
+        else {std::cout << "Te falta.\n";
+       }
+    }
+
+return 0;
+
+ }
